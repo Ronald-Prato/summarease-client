a735fbbb19b1491e3d59939b637dfa8cfca06f43
“dansaround” <kcomtdaniel@gmail.com>
2023-05-29 18:53:32 -0500

    recreated previous LoadingText with array of strings

--- src/components/LoadingText/LoadingText.tsx
+++ src/components/LoadingText/LoadingText.tsx
@@ -1,8 +1,30 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./LoadingText.module.css";
 
-export const LoadingText: FC<{ currentIndex: number }> = ({ currentIndex }) => {
-  const texts = ["Transcribiendo audio", "Generando Resumen"];
+export const LoadingText: FC = () => {
+  const texts = [
+    "Analizando Audio",
+    "Transcribiendo Audio",
+    "Generando Resumen",
+    "Creando Bullet Points",
+    "Buscando Links de Referencia",
+    "Convirtiendo ondas en bits",
+    "Entrenando a los hámsters generadores de resúmenes",
+    "Destilando la esencia del audio en palabras",
+    "Convocando a los enlaces mágicos de referencia",
+    "Enviando el resumen por lechuza",
+  ];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % texts.length);
+    }, 4000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [texts.length]);
 
   return (
     <div className={styles.textContainer}>
