d3260a6630b87ec6e3c3209ef955dbbd7e162b65
Ronald Prato <ronald.prato@unet.edu.ve>
2023-05-30 10:17:51 -0500

    wrong state declaration fixed

--- src/store/user/user.party.ts
+++ src/store/user/user.party.ts
@@ -1,31 +1,27 @@
-import produce from 'immer'
-import { _setUserData } from './user.actions'
-import { UserData } from '@/types/user'
+import produce from "immer";
+import { _setUserData } from "./user.actions";
+import { UserData } from "@/types/user";
 
 const initialState: UserData = {
-  elo: -1,
-  email: '',
-  nickname: '',
-  uid: '',
-  points: -1,
-  profilePic: '',
-}
+  email: "",
+  uid: "",
+};
 
-type UsersActions = ReturnType<typeof _setUserData>
+type UsersActions = ReturnType<typeof _setUserData>;
 
 const usersReducer = produce(
   (draft: UserData, action: UsersActions): UserData => {
-    const { type } = action
+    const { type } = action;
     switch (type) {
-      case 'users/SET_USER_DATA':
-        draft = action.payload
-        return draft
+      case "users/SET_USER_DATA":
+        draft = action.payload;
+        return draft;
 
       default:
-        return draft
+        return draft;
     }
   },
   initialState
-)
+);
 
-export { usersReducer }
+export { usersReducer };
